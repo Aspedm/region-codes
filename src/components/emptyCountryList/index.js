3d6d5d758bffe568beb630ac2ec1a674f5fd8ec6
Asped <aspedm@gmail.com>
2019-09-27 16:25:11 +0300

    fix typo

--- src/components/emptyCountryList/index.js
+++ src/components/emptyCountryList/index.js
@@ -4,7 +4,7 @@ import { Group } from '@vkontakte/vkui';
 const EmptyCountryList = () => (
     <Group 
         title="Список пуст" 
-        description="В списке представленны коды регинов, на сентябрь 2019 года." 
+        description="В списке представленны коды регионов, на сентябрь 2019 года." 
     />
 );
 
