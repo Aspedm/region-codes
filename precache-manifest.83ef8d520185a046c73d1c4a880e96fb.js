e2e67f51ad39285a2a93112e7212e2a245cd5997
Тагир Хаджиев <aspedm@gmail.com>
2019-11-05 20:02:03 +0300

    Updates

---
+++ precache-manifest.83ef8d520185a046c73d1c4a880e96fb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ec7f2e83ea15eaffff2cfe8a905819e",
+    "url": "/region-codes/static/media/license-plate-police.0ec7f2e8.png"
+  },
+  {
+    "revision": "31d1538faee3bfee6ae9",
+    "url": "/region-codes/static/css/main.e1ab0c55.chunk.css"
+  },
+  {
+    "revision": "4ece08f67d646629938b",
+    "url": "/region-codes/static/js/runtime~main.ed2b1104.js"
+  },
+  {
+    "revision": "488f872a8712056a511e9cc0039fdf8a",
+    "url": "/region-codes/static/media/roadnumbers.488f872a.woff"
+  },
+  {
+    "revision": "dcf9d8f6bdcf0d61e255",
+    "url": "/region-codes/static/js/2.eb1e96ee.chunk.js"
+  },
+  {
+    "revision": "b9f128467ea2ae312bd768f420c2bdcf",
+    "url": "/region-codes/static/media/license-plate-default.b9f12846.png"
+  },
+  {
+    "revision": "88d1aaf36bc4b56a6e980d3054d13df3",
+    "url": "/region-codes/static/media/license-plate-taxi.88d1aaf3.png"
+  },
+  {
+    "revision": "a8d3fb5ca0634206526327cdd9d76e7b",
+    "url": "/region-codes/static/media/license-plate-army.a8d3fb5c.png"
+  },
+  {
+    "revision": "fb5b44c1158ac6076b024e146a7613a6",
+    "url": "/region-codes/static/media/license-plate-diplomatic.fb5b44c1.png"
+  },
+  {
+    "revision": "31d1538faee3bfee6ae9",
+    "url": "/region-codes/static/js/main.51528c8d.chunk.js"
+  },
+  {
+    "revision": "8cfbc2aa7cabaf225790292efe6758d4",
+    "url": "/region-codes/static/media/license-plate-transit.8cfbc2aa.png"
+  },
+  {
+    "revision": "2ac462be8fcf20a83013e182fd093c25",
+    "url": "/region-codes/static/media/license-plate-default-2.2ac462be.png"
+  },
+  {
+    "revision": "0ff03f392a66049b1c40144c674c271f",
+    "url": "/region-codes/static/media/license-plate-tractor-2.0ff03f39.png"
+  },
+  {
+    "revision": "9517d2a01f55c78cf6e5d41334093a4a",
+    "url": "/region-codes/static/media/license-plate-moped.9517d2a0.png"
+  },
+  {
+    "revision": "cb79fd727cc3824b4a6d9749161aa7f6",
+    "url": "/region-codes/static/media/license-plate-antique.cb79fd72.png"
+  },
+  {
+    "revision": "f7039ed2350b365b4467d67a96ae52db",
+    "url": "/region-codes/static/media/license-plate-sport.f7039ed2.png"
+  },
+  {
+    "revision": "84d8e48f3b9e9a8f8223c4ada57dda44",
+    "url": "/region-codes/static/media/roadnumbers.84d8e48f.woff2"
+  },
+  {
+    "revision": "dcf9d8f6bdcf0d61e255",
+    "url": "/region-codes/static/css/2.58ffe350.chunk.css"
+  },
+  {
+    "revision": "b7268aa0c45073c01702ed170b6d867a",
+    "url": "/region-codes/index.html"
+  }
+];
